dbbce1d9e4a6d7460ad4912b61f02d64aa8a7321
riezmann <20bao.cg@vinuni.edu.vn>
2023-03-20 13:56:21 +0700

    feature: add category model, item model, user model

---
+++ main/models/ItemModel.py
@@ -0,0 +1,15 @@
+from main import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    created_time = db.Column(db.DateTime, nullable=False)
+    updated_time = db.Column(db.DateTime, nullable=False)
+    user = db.relationship("UserModel", back_populates="items")
+    category = db.relationship("CategoryModel", back_populates="items")
---
+++ main/models/UserModel.py
@@ -0,0 +1,14 @@
+from main import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(256), nullable=False)
+    salt = db.Column(db.String(32), nullable=False)
+    created_time = db.Column(db.DateTime, nullable=False)
+    updated_time = db.Column(db.DateTime, nullable=False)
+    items = db.relationship("ItemModel", back_populates="user", lazy="dynamic")
+    categories = db.relationship("CategoryModel", back_populates="user", lazy="dynamic")
